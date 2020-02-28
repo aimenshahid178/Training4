c1c3e4fbc667742c794f51afed5899c72ffaeed3
Aimen Shahid <ashahid@pk.folio3.com>
2020-02-28 18:23:46 +0500

    Login Validation

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-function login(){
+let users = ['aimen'];
+let passwords = ['123'];
+
+var input = document.getElementById("pwd");
+
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      document.getElementById("login").click();
+    }
+  });
+
+function login(user, password){
+    let found = false;
+    for(let i = 0; i < users.length; i++){
+        if(users[i] === user || passwords[i] === password){
+            window.location.replace("main.html");
+            found = true;
+        }
+    }
+    if(!found){
+        alert("Username or Password are incorrect. Please try again.");
+        reset();
+    }
 
 };
 
