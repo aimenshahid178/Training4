cf94cbc16ba7dd574a4cea2e4769da17ddb61469
Aimen Shahid <ashahid@pk.folio3.com>
2020-03-06 15:49:39 +0500

    Missing Attendance made dynamic

--- missing.js
+++ missing.js
@@ -1,6 +1,6 @@
-let intimes = ['in1', 'in2', 'in3', 'in4', 'in5', 'in6', 'in7'];
-let outtimes = ['out1', 'out2', 'out3', 'out4', 'out5', 'out6', 'out7'];
-let labels = ['l1', 'l2', 'l3', 'l4', 'l5', 'l6', 'l7'];
+let intimes = [];
+let outtimes = [];
+let labels = [];
 
 
 function updateDates(){
@@ -13,7 +13,7 @@ function updateDates(){
 
 function sendDates(){
     let datesToUpdate = [];
-    for (let i = 1; i <8; i++) {
+    for (let i = 0; i <labels.length; i++) {
          if(document.getElementById(i).checked == true) {
             let x = document.getElementById('l'+i);
             let y = document.getElementById('in'+i);
@@ -39,6 +39,51 @@ function sendDates(){
     }
 };
 
+function getDates(startDate, endDate) {
+    let dates = [],
+        currentDate = startDate,
+        addDays = function(days) {
+          let date = new Date(this.valueOf());
+          date.setDate(date.getDate() + days);
+          return date;
+        };
+    while (currentDate <= endDate) {
+      dates.push(currentDate);
+      currentDate = addDays.call(currentDate, 1);
+    }
+    return dates;
+  };
+
 function createForm(){
-    
-}
+    let startDate = document.getElementById('oldddate');
+    let endDate = document.getElementById('currddate');
+    let main_dates = getDates(startDate, endDate);
+    for (let i =0; i < main_dates.length; i++){
+        let newDiv = document.createElement('div');
+        let name = document.createElement('label');
+        let chkbox = document.createElement('input');
+        chkbox.type = 'checkbox';
+        let intime = document.createElement('input');
+        intime.type = 'text';
+        let outtime = document.createElement('input');
+        outtime.type = 'text';
+
+        newDiv.setAttribute('id',i);
+        name.setAttribute('id','l'+i);
+        chkbox.setAttribute('id', i);
+        intime.setAttribute('id', 'in'+i);
+        outtime.setAttribute('id', 'out'+i);
+
+        labels.push('l'+i);
+        intimes.push('in'+i);
+        outtimes.push('out'+i);
+
+        newDiv.appendChild(name);
+        newDiv.appendChild(chkbox);
+        newDiv.appendChild(intime);
+        newDiv.appendChild(outtime);
+    }
+};
+
+
+createForm();
